f00186547d6e146f353a4cbd4a4b7bc7ae8f14f9
Claudio153 <diazclaudio153@gmail.com>
2025-04-07 18:43:00 -0400

    dockerizacion y tests

---
+++ solemne1/src/solemne1/test_main.py
@@ -0,0 +1,24 @@
+import sys
+from datetime import datetime
+
+from fastapi.testclient import TestClient
+
+# Agregar `src` al PYTHONPATH en tiempo de ejecución
+sys.path.insert(0, "./src/solemne1")
+
+from main import app  # Ahora importamos directamente desde solemne1
+
+client = TestClient(app)
+
+
+def test_get_time():
+    response = client.get("/time")
+
+    data = response.json()
+    assert "current_time" in data  # Verifica que el JSON tiene la clave esperada
+
+    # Verifica que el formato del tiempo sea correcto
+    try:
+        datetime.strptime(data["current_time"], "%Y-%m-%d %H:%M:%S")
+    except ValueError:
+        assert False, "El formato de la fecha es incorrecto"
