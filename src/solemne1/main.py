d24e4db7ea27d716011db5038aa0dd7a45c3c046
vicentexte <vicentexte@gmail.com>
2025-04-08 14:07:56 -0400

    Update main.py

--- src/solemne1/main.py
+++ src/solemne1/main.py
@@ -10,4 +10,4 @@ async def get_time():
     return {"current_time": current_time}
 
 if __name__ == "__main__":
-    uvicorn.run(app,host="127.0.0.1", port=8000,reload=true)
+    uvicorn.run(app,host="127.0.0.1", port=8000,reload=True)
