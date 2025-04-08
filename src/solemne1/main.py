37d9f098ce2ebf1cfe548e0d903e72506eb0013e
vicentexte <vicentexte@gmail.com>
2025-04-08 10:25:58 -0400

    Update main.py

--- src/solemne1/main.py
+++ src/solemne1/main.py
@@ -3,11 +3,6 @@ from datetime import datetime
 
 app = FastAPI()
 
-
-@app.get("/")
-async def main_route():
-    return {"message": "Hey, It is me Goku"}
-
 @app.get("/time")
 async def get_time():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
