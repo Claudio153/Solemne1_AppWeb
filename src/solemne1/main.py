5fc632f835e22f507032e71865f862d6a30c1b25
vicentexte <vicentexte@gmail.com>
2025-04-08 10:48:36 -0400

    Update main.py

--- src/solemne1/main.py
+++ src/solemne1/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from datetime import datetime
+import uvicorn 
 
 app = FastAPI()
 
@@ -7,3 +8,6 @@ app = FastAPI()
 async def get_time():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return {"current_time": current_time}
+
+if __name__ == "__main__":
+    uvicorn.run(app,host="127.0.0.1", port=8000,reload=true)
